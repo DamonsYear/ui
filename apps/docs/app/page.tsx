46447288c12784caba9b604584ca97028875db6b
JengYoung <young_pallete@naver.com>
2023-06-25 19:16:01 +0900

    test(docs/page): success checkbox test

--- apps/docs/app/page.tsx
+++ apps/docs/app/page.tsx
@@ -3,48 +3,25 @@ import Box from "@damons-ui/box";
 import { Button } from "@damons-ui/button";
 import { CheckBox } from "@damons-ui/checkbox";
 import { HStack, VStack } from "@damons-ui/stack";
-import styled from "styled-components";
-
-const CheckLine = styled.polyline`
-  fill: none;
-  stroke: #ffffff;
-  stroke-width: 4px;
-  stroke-miterlimit: 10;
-  stroke-dashoffset: 0;
-  stroke-dasharray: 60;
-  transition: all 0.3s;
-  animation: dash 0.3s linear alternate 1;
-  @keyframes dash {
-    from {
-      stroke-dashoffset: 60;
-    }
-    to {
-      stroke-dashoffset: 0;
-    }
-  }
-`;
+import { useState } from "react";
 
 export default function Page() {
+  const [checked, setChecked] = useState(true);
+  const onChange = (value: boolean) => {
+    setChecked(value);
+  };
+
   return (
     <Box>
-      체크박스
-      <CheckBox></CheckBox>
-      <Box width="40px" height="40px" borderRadius="4px" background="#752bed">
-        <svg
-          width="100%"
-          height="100%"
-          xmlns="http://w3.org/2000/svg"
-          version="1.1"
-          viewBox="0 0 40 40"
-        >
-          <CheckLine
-            fill="none"
-            stroke="#fff"
-            strokeWidth="4px"
-            points="8 18 18 28 32 12"
-          />
-        </svg>
-      </Box>
+      <CheckBox
+        id="test"
+        checked={checked}
+        onChange={onChange}
+        activeBackground="#752bed"
+        defaultBackground="white"
+      >
+        CheckBox Click!
+      </CheckBox>
       <VStack spacing="10px">
         <Button>안녕하세요!</Button>
         <Button>반가워요!</Button>
