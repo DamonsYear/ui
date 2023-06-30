82bc4afbd6409ea2769fe2c664b848fc5c0520ed
JengYoung <young_pallete@naver.com>
2023-07-01 00:03:42 +0900

    refactor(Stack): refactor stack pseudo styles

--- packages/components/Stack/src/HStack.tsx
+++ packages/components/Stack/src/HStack.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import styled, { css } from "css-in-js";
-import { baseCSS } from "@damons-ui/react-core";
+import styled from "css-in-js";
+import { baseCSS, pseudoCSS } from "@damons-ui/react-core";
 import { TStack, TStyledStack } from "./types";
 
 /**
@@ -134,79 +134,19 @@ export function HStack({
 }
 
 HStack.Style = styled.div<TStyledStack>`
-  ${baseCSS}
+  ${(props) => baseCSS(props)}
 
+  ${(props) => props._hover && pseudoCSS(props._hover, ":hover")}
+  ${(props) => props._before && pseudoCSS(props._before, ":before")}
+  ${(props) => props._after && pseudoCSS(props._after, ":after")}
+  ${(props) => props._active && pseudoCSS(props._active, ":active")}
+  ${(props) => props._disabled && pseudoCSS(props._disabled, ":disabled")}
+  ${(props) => props._enabled && pseudoCSS(props._enabled, ":enabled")}
+  ${(props) => props._focus && pseudoCSS(props._focus, ":focus")}
   ${(props) =>
-    props._before &&
-    css`
-      &::before {
-        ${baseCSS}
-      }
-    `}
-
+    props._firstOfType && pseudoCSS(props._firstOfType, ":first-of-type")}
   ${(props) =>
-    props._after &&
-    css`
-      &::after {
-        ${baseCSS}
-      }
-    `}
-
-  ${(props) =>
-    props._active &&
-    css`
-      &:active {
-        ${baseCSS}
-      }
-    `}
-
-  ${(props) =>
-    props._disabled &&
-    css`
-      &:disabled {
-        ${baseCSS}
-      }
-    `}
-
-  ${(props) =>
-    props._enabled &&
-    css`
-      &:enabled {
-        ${baseCSS}
-      }
-    `}
-
-  ${(props) =>
-    props._focus &&
-    css`
-      &:focus {
-        ${baseCSS}
-      }
-    `}
-
-  ${(props) =>
-    props._hover &&
-    css`
-      &:hover {
-        ${baseCSS}
-      }
-    `}
-
-  ${(props) =>
-    props._firstOfType &&
-    css`
-      &:first-of-type {
-        ${baseCSS}
-      }
-    `}
-    
-  ${(props) =>
-    props._lastOfType &&
-    css`
-      &:last-of-type {
-        ${baseCSS}
-      }
-    `}
+    props._lastOfType && pseudoCSS(props._lastOfType, ":last-of-type")}
 
   * {
     &:not(:last-of-type) {
