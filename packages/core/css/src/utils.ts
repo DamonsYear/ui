e972e9d5c8f3d11794009dc5e8d9bc4677848d51
JengYoung <young_pallete@naver.com>
2023-08-20 00:07:55 +0900

    feat(@damons-ui/css-core): create TransitionCSSPropSet

---
+++ packages/core/css/src/utils.ts
@@ -0,0 +1,21 @@
+import { AdvancedPseudoSet, BasePseudoSet } from "./set";
+
+export const getPrefixDash = (s: string) => `-${s.toLowerCase()}`;
+
+const OneDashPrefixList = ["moz", "khtml", "webkit", "ms"];
+
+export const toSnakeCase = (char: string) => {
+  if (OneDashPrefixList.some((testCase) => char.startsWith(testCase))) {
+    return getPrefixDash(char).replace(/[A-Z]/g, getPrefixDash);
+  }
+
+  return char.replace(/[A-Z]/g, getPrefixDash);
+};
+
+export const transformStyleProp = (char: string) => {
+  if (BasePseudoSet.has(char) || AdvancedPseudoSet.has(char)) {
+    return toSnakeCase(char.replace(/_/g, (s) => `:${s}`));
+  }
+
+  return toSnakeCase(char);
+};
