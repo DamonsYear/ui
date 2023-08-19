4c78a220d5348df30c32dddaeb59ca86eaed93c4
JengYoung <young_pallete@naver.com>
2023-08-19 23:54:36 +0900

    feat(@damons-ui/css-core): create TextCSSPropSet

--- packages/core/css/src/bg.ts
+++ packages/core/css/src/bg.ts
@@ -1,6 +1,6 @@
 import { CSSProps } from "./types";
 
-export type Bg = Pick<
+export type BgCSSProps = Pick<
   CSSProps,
   | "background"
   | "backgroundColor"
@@ -19,7 +19,7 @@ export type Bg = Pick<
  * 강하게 타입을 추론시키고 싶지만 그러면 하위에서 타입단언을 매우 많이 해야 한다.
  * 따라서 불가피하게 string으로 자동 추론되게끔 설정했다.
  */
-export const BgPropSet = new Set<string>([
+export const BgCSSPropSet = new Set<string>([
   "background",
   "backgroundColor",
   "backgroundPosition",
--- packages/core/css/src/index.ts
+++ packages/core/css/src/index.ts
@@ -2,3 +2,4 @@ export * from "./unit";
 export * from "./types";
 export * from "./transform";
 export * from "./set";
+export * from "./text";
