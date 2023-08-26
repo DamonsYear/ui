46340f1d68304f2775c36c97a96e04d8644edbe7
JengYoung <young_pallete@naver.com>
2023-08-26 23:20:10 +0900

    refactor(@damons-ui/toast): refactor toast styles

--- packages/components/Toast/Fixed/FixedToast.style.ts
+++ packages/components/Toast/Fixed/FixedToast.style.ts
@@ -1,4 +1,4 @@
-import { CSSProps, baseCSS, pseudoCSS } from "@damons-ui/react-core";
+import { CSSProps, basePropsCSS } from "@damons-ui/react-core";
 import styled from "css-in-js";
 
 export const Container = styled.section<CSSProps>`
@@ -8,33 +8,11 @@ export const Container = styled.section<CSSProps>`
   justify-content: center;
   align-items: center;
 
-  ${baseCSS}
-  ${(props) => props._hover && pseudoCSS(props._hover, ":hover")}
-  ${(props) => props._before && pseudoCSS(props._before, ":before")}
-  ${(props) => props._after && pseudoCSS(props._after, ":after")}
-  ${(props) => props._active && pseudoCSS(props._active, ":active")}
-  ${(props) => props._disabled && pseudoCSS(props._disabled, ":disabled")}
-  ${(props) => props._enabled && pseudoCSS(props._enabled, ":enabled")}
-  ${(props) => props._focus && pseudoCSS(props._focus, ":focus")}
-  ${(props) =>
-    props._firstOfType && pseudoCSS(props._firstOfType, ":first-of-type")}
-  ${(props) =>
-    props._lastOfType && pseudoCSS(props._lastOfType, ":last-of-type")}
+  ${basePropsCSS}
 `;
 
 export const Text = styled.p`
   margin: 0;
 
-  ${baseCSS}
-  ${(props) => props._hover && pseudoCSS(props._hover, ":hover")}
-  ${(props) => props._before && pseudoCSS(props._before, ":before")}
-  ${(props) => props._after && pseudoCSS(props._after, ":after")}
-  ${(props) => props._active && pseudoCSS(props._active, ":active")}
-  ${(props) => props._disabled && pseudoCSS(props._disabled, ":disabled")}
-  ${(props) => props._enabled && pseudoCSS(props._enabled, ":enabled")}
-  ${(props) => props._focus && pseudoCSS(props._focus, ":focus")}
-  ${(props) =>
-    props._firstOfType && pseudoCSS(props._firstOfType, ":first-of-type")}
-  ${(props) =>
-    props._lastOfType && pseudoCSS(props._lastOfType, ":last-of-type")}
+  ${basePropsCSS}
 `;
